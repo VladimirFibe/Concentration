6c15ba536f5e25e733bd8666e6a1e504b29aa740
Vladimir Fibe <motiw@icloud.com>
2022-03-27 08:40:31 +0600

    add emojiChoices and cardButtons

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,14 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-  @IBAction func touchCard(_ sender: UIButton) {
-    print(sender.titleLabel?.text)
+  var flipCount = 0 {
+    didSet { flipCountLabel.text = "\(flipCount)"}
   }
   
-
+  var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+  @IBOutlet weak var flipCountLabel: UILabel!
+  
+  @IBOutlet var cardButtons: [UIButton]!
+  
+  func configureUI() {
+    
+  }
+  
+  @IBAction func touchCard(_ sender: UIButton) {
+    flipCount += 1
+    if let cardNumber = cardButtons.firstIndex(of: sender) {
+      flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+    } else {
+      print("chosen card was not in cardButtons")
+    }
+  }
+  
+  func flipCard(withEmoji emoji: String, on button: UIButton) {
+    if button.currentTitle == emoji {
+      button.setTitle("", for: .normal)
+      button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+    } else {
+      button.setTitle(emoji, for: .normal)
+      button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+  }
 }
 
